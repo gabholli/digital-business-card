4ff4cc097a7754cb0a0d0cfa944107251de76972
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2023-09-12 00:55:43 -0700

    Changed divs to section tags
    Changes divs to section tags in About.js and Interests.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,13 @@ import React from "react"
 
 function About() {
     return (
-        <div>
+        <section>
             <h2>Test</h2>
             <p>I am a frontend developer with a particular interest
                 in making things simple and automating daily tasks.
                 I try to keep up with security and best practices,
                 and am always looking for new things to learn.</p>
-        </div>
+        </section>
     )
 }
 
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,12 +2,12 @@ import React from "react"
 
 function Interests() {
     return (
-        <div>
+        <section>
             <h2>Interests</h2>
             <p>Food expert. Music scholar. Reader.
                 Internet fanatic. Bacon buff. Entrepreneur.
                 Travel geek. Pop culture ninja. Coffee fanatic.</p>
-        </div>
+        </section>
     )
 }
 
